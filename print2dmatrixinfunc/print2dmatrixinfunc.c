df25a11d16765b7115a4f8e9a8dbc9c1e429dfbe
hanbyeol Chae <hanbyeol4869@gmail.com>
2021-11-21 22:49:52 +0900

    ok

---
+++ print2dmatrixinfunc/print2dmatrixinfunc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define M 2
+#define N 3
+
+void printM2Da(int[][N]);
+void printM2Dp(int(*)[N]);
+
+int main(void) {
+	int B[M][N] = { {1,0,0},{0,1,0}, };
+
+	printM2Dp(B);
+	printM2Da(B);
+
+	return 0;
+}
+
+void printM2Da(int a[][N]) {
+	int i, j;
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void printM2Dp(int(*a)[N]) {
+	int i, j;
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N; j++) {
+			printf("%d ", (*(a + i))[j]);
+		}
+		printf("\n");
+	}
+}
